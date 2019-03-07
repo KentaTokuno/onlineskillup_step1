301b1b2924ff8230070841740c5d50876b4176d9
KentaTokuno <f118608@ed.socu.ac.jp>
2019-03-07 20:22:07 +0900

    最初のコミット

--- app/index.php
+++ app/index.php
@@ -2,24 +2,12 @@
 <html>
   <head>
     <meta charset="UTF-8" />
-    <title>POSTのサンプル</title>
+    <title>PHPのサンプル</title>
   </head>
   <body>
     <?php
-      //commentがPOSTされているなら
-      if(isset($_POST["comment"])){
-        //エスケープしてから表示
-        $comment = htmlspecialchars($_POST["comment"]);
-        print("あなたのコメントは「 ${comment} 」です。");
-      } else {
-    ?>
-        <p>コメントしてください。</p>
-        <form method="POST" action="index.php">
-          <input name="comment" />
-          <input type="submit" value="送信" />
-        </form>
-    <?php
-      }
+      $date = date("Y/m/d H:i:s");
+      print($date);
     ?>
   </body>
 </html>
